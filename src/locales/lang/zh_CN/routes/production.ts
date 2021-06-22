8b2b69acdb291df6017c03aece5480ac91c9a78f
luofangwei <1462449970@qq.com>
2021-06-22 13:49:03 +0800

    refactor: init project

---
+++ src/locales/lang/zh_CN/routes/production.ts
@@ -0,0 +1,3 @@
+export default {
+  production: '商品管理',
+};
---
+++ src/views/production/index.vue
@@ -0,0 +1,134 @@
+<template>
+  <PageWrapper>
+    <a-card hoverable class="enter-y">
+      <h1> 商品管理 </h1>
+      <!-- <a-card-meta title="Card title" description="This is the description">
+        <template #avatar>
+          <a-avatar
+            src="https://avatars.githubusercontent.com/u/42991514?s=400&u=db92068f4c716cb82dd4737ea5d4848408188abe&v=4"
+          />
+        </template>
+      </a-card-meta> -->
+      <div> </div>
+    </a-card>
+
+    <!-- 表格 -->
+    <div class="">
+      <a-card hoverable class="enter-y" title="商品表格">
+        <a-table :columns="columns" :data-source="data">
+          <template #name="{ text }">
+            <a>{{ text }}</a>
+          </template>
+          <template #customTitle>
+            <span>
+              <smile-outlined />
+              Name
+            </span>
+          </template>
+          <template #tags="{ text: tags }">
+            <span>
+              <a-tag
+                v-for="tag in tags"
+                :key="tag"
+                :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'"
+              >
+                {{ tag.toUpperCase() }}
+              </a-tag>
+            </span>
+          </template>
+          <template #action="{ record }">
+            <span>
+              <a>Invite 一 {{ record.name }}</a>
+              <a-divider type="vertical" />
+              <a>Delete</a>
+              <a-divider type="vertical" />
+              <a class="ant-dropdown-link">
+                More actions
+                <down-outlined />
+              </a>
+            </span>
+          </template>
+        </a-table>
+      </a-card>
+    </div>
+  </PageWrapper>
+</template>
+
+<script lang="ts">
+  import { defineComponent, ref } from 'vue';
+  import { PageWrapper } from '/@/components/Page';
+  import { SmileOutlined, DownOutlined } from '@ant-design/icons-vue';
+  const columns = [
+    {
+      dataIndex: 'name',
+      key: 'name',
+      slots: { title: 'customTitle', customRender: 'name' },
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: 'Tags',
+      key: 'tags',
+      dataIndex: 'tags',
+      slots: { customRender: 'tags' },
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      slots: { customRender: 'action' },
+    },
+  ];
+
+  const data = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      tags: ['nice', 'developer'],
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      tags: ['loser'],
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+  ];
+  export default defineComponent({
+    components: {
+      PageWrapper,
+      SmileOutlined,
+      DownOutlined,
+    },
+    setup() {
+      const loading = ref(true);
+
+      setTimeout(() => {
+        loading.value = false;
+      }, 1500);
+      return {
+        loading,
+        columns,
+        data,
+      };
+    },
+  });
+</script>
+
+<style></style>
