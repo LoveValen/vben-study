79e4326aeb381aafb294cbcaa2ee042466301e3f
luofangwei <1462449970@qq.com>
2021-08-30 18:01:44 +0800

    样式引入

--- src/views/production/index.vue
+++ src/views/production/index.vue
@@ -3,7 +3,7 @@
     <a-button type="primary" @click="handleClick"> 点击跳转到详情页 </a-button>
     <a-card hoverable class="enter-y">
       <h1> 商品管理 </h1>
-      <div> </div>
+      <div ref="chartRef" :style="{ width, height }"></div>
     </a-card>
 
     <!-- 表格 -->
@@ -49,91 +49,214 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent, ref } from 'vue';
-  import { PageWrapper } from '/@/components/Page';
-  import { SmileOutlined, DownOutlined } from '@ant-design/icons-vue';
-  import { useRouter } from 'vue-router';
-  const columns = [
-    {
-      dataIndex: 'name',
-      key: 'name',
-      slots: { title: 'customTitle', customRender: 'name' },
-    },
-    {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-    },
-    {
-      title: 'Tags',
-      key: 'tags',
-      dataIndex: 'tags',
-      slots: { customRender: 'tags' },
-    },
-    {
-      title: 'Action',
-      key: 'action',
-      slots: { customRender: 'action' },
-    },
-  ];
+import { defineComponent, onMounted, Ref, ref } from 'vue';
+import { PageWrapper } from '/@/components/Page';
+import { SmileOutlined, DownOutlined } from '@ant-design/icons-vue';
+import { useRouter } from 'vue-router';
+import { useECharts } from '/@/hooks/web/useECharts';
+const columns = [
+  {
+    dataIndex: 'name',
+    key: 'name',
+    slots: { title: 'customTitle', customRender: 'name' },
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    slots: { customRender: 'tags' },
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    slots: { customRender: 'action' },
+  },
+];
 
-  const data = [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-      tags: ['nice', 'developer'],
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-      tags: ['loser'],
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park',
-      tags: ['cool', 'teacher'],
-    },
-  ];
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
 
-  export default defineComponent({
-    components: {
-      PageWrapper,
-      SmileOutlined,
-      DownOutlined,
+export default defineComponent({
+  components: {
+    PageWrapper,
+    SmileOutlined,
+    DownOutlined,
+  },
+
+  props: {
+    width: {
+      type: String as PropType<string>,
+      default: '100%',
     },
-
-    setup() {
-      const loading = ref(true);
-      const router = useRouter();
-
-      function handleClick() {
-        router.push({
-          path: '/erabbit/home',
-        });
-      }
-
-      setTimeout(() => {
-        loading.value = false;
-      }, 1500);
-      return {
-        loading,
-        columns,
-        data,
-        handleClick,
-      };
+    height: {
+      type: String as PropType<string>,
+      default: '300px',
     },
-  });
+  },
+
+  setup() {
+    const loading = ref(true);
+    const router = useRouter();
+
+    const chartRef = ref<HTMLDivElement | null>(null);
+    const { setOptions } = useECharts(chartRef as Ref<HTMLDivElement>);
+
+    function handleClick() {
+      router.push({
+        path: '/erabbit/home',
+      });
+    }
+
+    onMounted(() => {
+      setOptions({
+        title: {
+          text: '堆叠区域图',
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#6a7985',
+            },
+          },
+        },
+        legend: {
+          data: [
+            {
+              name: '直接访问',
+              // 强制设置图形为圆。
+              icon: 'circle',
+              itemStyle: {
+                color: 'red',
+              },
+              lineStyle: {},
+            },
+            {
+              name: '联盟广告',
+              // 强制设置图形为圆。
+              icon: 'circle',
+              itemStyle: {
+                color: 'yellow',
+              },
+            },
+            {
+              name: '视频广告',
+              // 强制设置图形为圆。
+              icon: 'circle',
+              itemStyle: {
+                color: 'green',
+              },
+            },
+          ],
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {},
+          },
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true,
+        },
+        xAxis: [
+          {
+            type: 'category',
+            boundaryGap: false,
+            data: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+          },
+        ],
+        yAxis: [
+          {
+            type: 'value',
+          },
+        ],
+        series: [
+          {
+            name: '直接访问',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {
+              color: 'green',
+            },
+            emphasis: {
+              focus: 'series',
+            },
+            data: [0, 332, 301, 334, 390, 330, 320],
+          },
+          {
+            name: '联盟广告',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {
+              color: 'yellow',
+            },
+            emphasis: {
+              focus: 'series',
+            },
+            data: [0, 182, 191, 234, 290, 330, 310],
+          },
+          {
+            name: '视频广告',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {
+              color: 'red',
+            },
+            emphasis: {
+              focus: 'series',
+            },
+            data: [0, 232, 201, 154, 190, 330, 410],
+          },
+        ],
+      });
+    });
+
+    setTimeout(() => {
+      loading.value = false;
+    }, 1500);
+    return {
+      loading,
+      columns,
+      data,
+      chartRef,
+      handleClick,
+    };
+  },
+});
 </script>
 
 <style></style>
--- src/views/xtx_layout/default/header/index.vue
+++ src/views/xtx_layout/default/header/index.vue
@@ -37,192 +37,191 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent, ref } from 'vue';
-  import { SearchOutlined } from '@ant-design/icons-vue';
-  import cartSvg from '/@/assets/svg/cart.svg';
-  const navList = [
-    {
-      id: 0,
-      text: '首页',
-    },
-    {
-      id: 1,
-      text: '美食',
-    },
-    {
-      id: 2,
-      text: '餐厨',
-    },
-    {
-      id: 3,
-      text: '艺术',
-    },
-    {
-      id: 4,
-      text: '电器',
-    },
-    {
-      id: 5,
-      text: '居家',
-    },
-    {
-      id: 6,
-      text: '洗护',
-    },
-    {
-      id: 7,
-      text: '孕婴',
-    },
-    {
-      id: 8,
-      text: '服装',
-    },
-    {
-      id: 9,
-      text: '杂项',
-    },
-  ];
-  export default defineComponent({
-    components: {
-      SearchOutlined,
-    },
-    setup() {
-      let showHeader = ref(false);
-      const enterHandler = function (id: number): void {
-        if(id === 0) return
-        showHeader.value = true;
-      };
-      const leaveHandler = function (id: number): void {
-        if(id === 0) return
-        showHeader.value = false;
-      };
-      return {
-        navList,
-        cartSvg,
-        showHeader,
-        enterHandler,
-        leaveHandler,
-      };
-    },
-  });
+import { defineComponent, ref } from 'vue';
+import { SearchOutlined } from '@ant-design/icons-vue';
+import cartSvg from '/@/assets/svg/cart.svg';
+const navList = [
+  {
+    id: 0,
+    text: '首页',
+  },
+  {
+    id: 1,
+    text: '美食',
+  },
+  {
+    id: 2,
+    text: '餐厨',
+  },
+  {
+    id: 3,
+    text: '艺术',
+  },
+  {
+    id: 4,
+    text: '电器',
+  },
+  {
+    id: 5,
+    text: '居家',
+  },
+  {
+    id: 6,
+    text: '洗护',
+  },
+  {
+    id: 7,
+    text: '孕婴',
+  },
+  {
+    id: 8,
+    text: '服装',
+  },
+  {
+    id: 9,
+    text: '杂项',
+  },
+];
+export default defineComponent({
+  components: {
+    SearchOutlined,
+  },
+  setup() {
+    let showHeader = ref(false);
+    const enterHandler = function (id: number): void {
+      if (id === 0) return;
+      showHeader.value = true;
+    };
+    const leaveHandler = function (id: number): void {
+      if (id === 0) return;
+      showHeader.value = false;
+    };
+    return {
+      navList,
+      cartSvg,
+      showHeader,
+      enterHandler,
+      leaveHandler,
+    };
+  },
+});
 </script>
 
 <style lang="less" scoped>
-  @import '/@/design/xtx_pc/variables.less';
-  .app-header {
-    background-color: #fff;
-    .container {
-      position: relative;
+.app-header {
+  background-color: #fff;
+  .container {
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .app-logo {
+      width: 200px;
+      a {
+        display: block;
+        width: 100%;
+        height: 130px;
+        background: url('/@/design/images/logo.png') no-repeat center;
+        background-size: 100%;
+        color: #fff;
+      }
+    }
+    .app-header-nav {
       display: flex;
-      justify-content: space-between;
+      justify-content: space-evenly;
       align-items: center;
-      .app-logo {
-        width: 200px;
+      li {
+        position: relative;
         a {
           display: block;
-          width: 100%;
-          height: 130px;
-          background: url('/@/design/images/logo.png') no-repeat center;
-          background-size: 100%;
-          color: #fff;
-        }
-      }
-      .app-header-nav {
-        display: flex;
-        justify-content: space-evenly;
-        align-items: center;
-        li {
-          position: relative;
-          a {
-            display: block;
-            padding: 15px;
-            margin: 0 10px;
+          padding: 15px;
+          margin: 0 10px;
+          &::after {
+            position: absolute;
+            content: '';
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 1px;
+            background: @xtxColor;
+            transform: scale(0);
+          }
+          &:hover {
+            color: @xtxColor;
             &::after {
-              position: absolute;
-              content: '';
-              bottom: 0;
-              left: 0;
-              width: 100%;
-              height: 1px;
-              background: @xtxColor;
-              transform: scale(0);
-            }
-            &:hover {
-              color: @xtxColor;
-              &::after {
-                transition: all 0.5s;
-                transform: scale(0.5);
-              }
+              transition: all 0.5s;
+              transform: scale(0.5);
             }
           }
-        }
-      }
-      .app-header-diolog {
-        opacity: 0;
-        position: absolute;
-        top: 110px;
-        left: 0;
-        display: flex;
-        align-items: center;
-        justify-content: start;
-        width: 100%;
-        height: 0px;
-        background: #fff;
-        box-shadow: 0 3px 10px 3px rgb(0 0 0 / 6%);
-        .app-header-img-nav {
-          width: 100px;
-          height: 100px;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: space-around;
-          img {
-            width: 70px;
-            height: 70px;
-          }
-        }
-      }
-      .show-diolog {
-        opacity: 1;
-        height: 130px;
-        transition: all 0.5s;
-      }
-      .app-search {
-        position: relative;
-        :deep(.anticon) {
-          position: absolute;
-          top: 7px;
-          left: 3px;
-          font-size: 18px;
-        }
-        &-input {
-          padding: 6px 4px;
-          padding-left: 24px;
-          border-bottom: 1px solid #e7e7e7;
-        }
-      }
-      .app-cart {
-        position: relative;
-        img {
-          width: 30px;
-          background: transparent;
-        }
-        .cart-order {
-          position: absolute;
-          top: 0;
-          left: 20px;
-          z-index: 9;
-          height: 12px;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          font-size: 12px;
-          padding: 3px 5px;
-          border-radius: 8px;
-          background: @priceColor;
-          color: #fff;
         }
       }
     }
+    .app-header-diolog {
+      opacity: 0;
+      position: absolute;
+      top: 110px;
+      left: 0;
+      display: flex;
+      align-items: center;
+      justify-content: start;
+      width: 100%;
+      height: 0px;
+      background: #fff;
+      box-shadow: 0 3px 10px 3px rgb(0 0 0 / 6%);
+      .app-header-img-nav {
+        width: 100px;
+        height: 100px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-around;
+        img {
+          width: 70px;
+          height: 70px;
+        }
+      }
+    }
+    .show-diolog {
+      opacity: 1;
+      height: 130px;
+      transition: all 0.5s;
+    }
+    .app-search {
+      position: relative;
+      :deep(.anticon) {
+        position: absolute;
+        top: 7px;
+        left: 3px;
+        font-size: 18px;
+      }
+      &-input {
+        padding: 6px 4px;
+        padding-left: 24px;
+        border-bottom: 1px solid #e7e7e7;
+      }
+    }
+    .app-cart {
+      position: relative;
+      img {
+        width: 30px;
+        background: transparent;
+      }
+      .cart-order {
+        position: absolute;
+        top: 0;
+        left: 20px;
+        z-index: 9;
+        height: 12px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 12px;
+        padding: 3px 5px;
+        border-radius: 8px;
+        background: @priceColor;
+        color: #fff;
+      }
+    }
   }
+}
 </style>
